6fbd3ad0afed044971bbd0b4d014594b4ddfb3fc
eric <eric.razorjr@gmail.com>
2020-09-23 23:51:55 -0400

    Add post action to pantry

--- src/reducers/pantryInput.js
+++ src/reducers/pantryInput.js
@@ -4,13 +4,14 @@ const initialState = {
 
 export default (state=initialState, action) => {
   switch (action.type) {
-    case "UPDATE_PANTRY":
+    case "UPDATE_PANTRY_INPUT":
       return {
         ...state,
         [action.pantryInputData.name]: action.pantryInputData.value
       }
-      case "POST_PANTRY_ITEM":
-        return state
+      case "ADD_TO_PANTRY":
+        debugger
+        return state.concat(action.item)
     default:
       return state
   }
